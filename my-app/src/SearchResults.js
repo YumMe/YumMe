0aad3f22df32f8abace5daf9d656dfd277578b0c
Melody Lee <leemeli@uw.edu>
2017-05-15 11:05:56 -0700

    Re-initialize search results component

---
+++ my-app/src/SearchResults.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class SearchResults extends Component {
+  render() {
+    return (
+      <div className="title">
+        YumMe! This is the search results page
+      </div>
+    );
+  }
+}
+
+export default SearchResults;

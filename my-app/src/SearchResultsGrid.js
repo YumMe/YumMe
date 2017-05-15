10ed542f032f63b4176cc17f8f803445776846cb
Melody Lee <leemeli@uw.edu>
2017-05-15 11:00:27 -0700

    Initialize search grid and search square components

--- my-app/src/SearchResultsGrid.js
+++ my-app/src/SearchResultsGrid.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 
-class App extends Component {
+class SearchGrid extends Component {
   render() {
     return (
       <div className="title">
-        YumMe! This is the serach results page
+        YumMe! This is the search results page
       </div>
     );
   }
 }
 
-export default App;
+export default SearchGrid;

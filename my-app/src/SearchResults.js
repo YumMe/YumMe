0ca5eb1d672e84f03afd255ab352d2d39b5323a1
Melody Lee <leemeli@uw.edu>
2017-05-17 10:05:24 -0700

    Edit Yelp API code

--- my-app/src/SearchResults.js
+++ my-app/src/SearchResults.js
@@ -6,18 +6,27 @@ class SearchResults extends Component {
 
     constructor(props) {
         super(props);
-        this.componentWillReceiveProps();
     }
 
-    componentWillReceiveProps() {
+    componentWillMount() {
         const yelp = require('yelp-fusion');
-        const token = {"token":"mFx9GawCOq_cHnJvJ03aPdCGXyZMQCluSNVVFJ6Exb9nftJaIUTyXiukKT19_86n3P13wV_lbaNOyxUhoyXspWEWPeJ-8YjKIzDzHp9yPaY3zb2IAHs5f5adRl8WWXYx"};
-        const client = yelp.client(token);
-        client.search({
-            term:'Four Barrel Coffee',
-            location: 'san francisco, ca'
-        }).then(response => {
-            console.log(response.jsonBody.businesses[0].name);
+        // Place holders for Yelp Fusion's OAuth 2.0 credentials. Grab them
+        // from https://www.yelp.com/developers/v3/manage_app
+        const clientId = '-TEl59_tdP6IVt5oFziseA';
+        const clientSecret = 'UiRibnj9ucjfMQU54nNc0IF3Ad2CqW8m7L7XXq3nfZhgbPpzoLJw1XslfRPpWUFH';
+
+        const searchRequest = {
+            term:'Meesum',
+            location: 'seattle, wa'
+        };
+
+        yelp.accessToken(clientId, clientSecret).then(response => {
+        const client = yelp.client(response.jsonBody.access_token);
+        client.search(searchRequest).then(response => {
+            const firstResult = response.jsonBody.businesses[0];
+            const prettyJson = JSON.stringify(firstResult, null, 4);
+            console.log(prettyJson);
+        });
         }).catch(e => {
             console.log(e);
         });

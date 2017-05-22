723915bfb4f4c593a48aba59834a25c8bebb51a9
greycabb <greycabbage@gmail.com>
2017-05-22 11:50:42 -0700

    added query parameters to search results page from the front page

--- my-app/src/SearchBar.js
+++ my-app/src/SearchBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { hashHistory } from 'react-router';
 
+
 /*
     ping @michael if you have questions
 
@@ -203,15 +204,38 @@ export default class SearchBar extends React.Component {
 
         var usingCurrentLocation = false;
 
+        // Parameters:
+
+        // 1) city = city name
+        //      ignored if lat and long are specified
+        // 2) lat = latitude
+        // 3) long = longitude
+        //      ignored if mylocation = false
+
+        var lat = '';
+        if (this.state.latitude !== '') {
+            lat = this.state.latitude;
+        }
+        var long = '';
+        if (this.state.longitude !== '') {
+            long = this.state.longitude;
+        }
+        var city = '';
+        if (this.state.search.trim() !== '') {
+            city = this.state.search;
+        } else {
+            usingCurrentLocation = true;
+        }
+
         if (usingCurrentLocation === true) {
             console.log('Going to search results page for current location');
-            hashHistory.push('/searchresults');
+            hashHistory.push('/search?lat=' + lat + '&long=' + long);
         } else if (usingCurrentLocation === false) {
             console.log('Going to search results page for "' + this.state.search + '"');
-            hashHistory.push('searchresults');
+            hashHistory.push('/search?city=' + this.state.search);
         }
-        hashHistory.push('searchresults');
     }
+
 
 
     // Render in dom
--- my-app/src/SearchResults.js
+++ my-app/src/SearchResults.js
@@ -4,37 +4,123 @@ import logo from './logo.svg';
 
 class SearchResults extends Component {
 
-    constructor(props) {
-        super(props);
-        this.componentDidlMount();
+  constructor(props) {
+    super(props);
+    this.componentDidlMount();
+    this.queryParametersAreValid = this.queryParametersAreValid.bind(this);
+  }
+
+  componentDidlMount() {
+    var data = "client_id=-TEl59_tdP6IVt5oFziseA&client_secret=UiRibnj9ucjfMQU54nNc0IF3Ad2CqW8m7L7XXq3nfZhgbPpzoLJw1XslfRPpWUFH&grant_type=client_credentials";
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
+      }
+    });
+
+    xhr.open("GET", "https://api.yelp.com/v3/businesses/search?term=Meesum&location=seattle%2C%20wa");
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("authorization", "Bearer mFx9GawCOq_cHnJvJ03aPdCGXyZMQCluSNVVFJ6Exb9nftJaIUTyXiukKT19_86n3P13wV_lbaNOyxUhoyXspWEWPeJ-8YjKIzDzHp9yPaY3zb2IAHs5f5adRl8WWXYx");
+    xhr.setRequestHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+    xhr.setRequestHeader("cache-control", "no-cache");
+    xhr.setRequestHeader("postman-token", "13dc087b-69b6-539d-241b-9f0c12423a19");
+
+    xhr.send(data);
+  }
+
+
+  // Validates query parameters (optional, add this if necessary)
+  queryParametersAreValid(lat, long) {
+
+    // If there is a city
+    // Latitude should be a number
+    if (lat !== undefined) {
+      if (long === undefined) {
+        // If there is a lat, there must be a long, so this is wrong
+        return false;
+      }
     }
 
-    componentDidlMount() {
-       var data = "client_id=-TEl59_tdP6IVt5oFziseA&client_secret=UiRibnj9ucjfMQU54nNc0IF3Ad2CqW8m7L7XXq3nfZhgbPpzoLJw1XslfRPpWUFH&grant_type=client_credentials";
+    var isNumbers = new RegExp('/^\d+$/');
 
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
+    var validLat = lat.match(isNumbers) && lat.length > 0;
+    var validLong = long.match(isNumbers) && lat.length > 0;
 
-        xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === 4) {
-            console.log(this.responseText);
-        }
-        });
-
-        xhr.open("GET", "https://api.yelp.com/v3/businesses/search?term=Meesum&location=seattle%2C%20wa");
-        xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-        xhr.setRequestHeader("authorization", "Bearer mFx9GawCOq_cHnJvJ03aPdCGXyZMQCluSNVVFJ6Exb9nftJaIUTyXiukKT19_86n3P13wV_lbaNOyxUhoyXspWEWPeJ-8YjKIzDzHp9yPaY3zb2IAHs5f5adRl8WWXYx");
-        xhr.setRequestHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
-        xhr.setRequestHeader("cache-control", "no-cache");
-        xhr.setRequestHeader("postman-token", "13dc087b-69b6-539d-241b-9f0c12423a19");
-
-        xhr.send(data);
+    // If both lat and long are only numbers
+    if (validLat && validLong) {
+      return true;
     }
+    return false;
+  }
+
 
   render() {
+
+    console.log('memes');
+    console.log(this.props.location.query);
+
+    var query = this.props.location.query;
+
+
+    var valid = true;
+
+    var searchType = ''; // 'coords' or 'city'
+
+    if (query !== undefined) {
+
+      
+
+      // 1) Latitude and longitude, takes priority (current position)
+      if (query.lat !== undefined && query.long !== undefined) {
+        console.log('Latitude: ' + query.lat);
+        console.log('Longitude: ' + query.long);
+
+        // Validate long, lat
+        if (!this.queryParametersAreValid(query.lat, query.long)) {
+          valid = false;
+        } else {
+          searchType = 'coords';
+        }
+      }
+
+      // 2) OR Specified city
+      else if (query.city !== undefined) {
+        console.log('City: ' + query.city);
+        searchType = 'city';
+      }
+      // 3) OR errors in query parameters
+      else {
+        console.log('invalid or missing query parameters');
+        valid = false;
+      }
+    }
+
+    var queryInfoDiv = (<div></div>);
+
+    if (valid) {
+      if (searchType === 'city') {
+        queryInfoDiv = (
+          <div>{this.props.location.query.city}</div>
+        );
+      } else if (searchType === 'coords') {
+        queryInfoDiv = (
+          <div>
+            <div>{this.props.location.query.lat}</div>
+            <div>{this.props.location.query.long}</div>
+          </div>
+        );
+      }
+    }
+    // Default: current location
+
     return (
       <div className="title">
         YumMe! This is the search results page
+        {queryInfoDiv}
       </div>
     );
   }

bb1b4602bd3d57a5bf6d2a41c7e589f6b52b78a6
Melody Lee <leemeli@uw.edu>
2017-05-24 13:37:00 -0700

    small comments

--- my-app/src/SearchSquare.js
+++ my-app/src/SearchSquare.js
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 class SearchSquare extends Component {
   render() {
     return (
+      // All of this still needs to be properly formatted 
       <div className="searchSquare">
         <img src ={this.props.image} />
         The venue id: {this.props.venueId}
